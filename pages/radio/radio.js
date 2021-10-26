fa786495fe39de4dd29c7c807da18d2747db760e
yxs <asong0122@163.com>
2021-10-26 22:57:35 +0800

    小程序轮播图学习

---
+++ pages/radio/radio.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    gender: ""
+  },
+  handleChange(e) {
+    // 1 获取单选框中的值
+    let gender = e.detail.value
+    // 2 把值 赋值给 data中的数据
+    this.setData({
+      gender
+    })
+  }
+})
